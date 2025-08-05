fe1336cc43d532a79ead083223aaed7fd514817f
ch-li <smling9_9@hotmail.com>
2025-08-05 19:18:08 +0100

    add logic to check null.

--- src/main/java/io/github/smling/metofficesdk/core/BaseClient.java
+++ src/main/java/io/github/smling/metofficesdk/core/BaseClient.java
@@ -5,18 +5,18 @@ import reactor.netty.http.client.HttpClient;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.Objects;
 
 public abstract class BaseClient {
     protected HttpClient getHttpClient(SdkSettings sdkSettings) {
+        if(Objects.isNull(sdkSettings.getApiKey())) {
+            throw new IllegalArgumentException("API key could not be null");
+        }
         return HttpClient.create()
                 .baseUrl(sdkSettings.getBaseUrl())
                 .headers(headers -> {
                     headers.add("accept", "application/json");
                     headers.add("apikey", sdkSettings.getApiKey());
-                })
-                .httpResponseDecoder(spec -> {
-                    spec.h2cMaxContentLength(sdkSettings.getMaxResponseBodySizeInKb() * 1024);
-                    return spec;
                 });
     }
 
